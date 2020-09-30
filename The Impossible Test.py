bfad997a9c6ae5199745a0fac7192794693d48ad
MiyukiHoshi21 <69836684+MiyukiHoshi21@users.noreply.github.com>
2020-09-30 10:11:56 +0800

    Update The Impossible Test.py

--- The Impossible Test.py
+++ The Impossible Test.py
@@ -3,14 +3,17 @@ print()
 
 counter = 0
 score = 0
-total_num_of_qn = 100
+total_num_of_qn = 7
 
 
 counter +=1
 tracker = 0
 
 while tracker !=1:
-  print("Geography Questions")
+  
+  print("---------------------------------------")
+  print("~~~~~~~~~~~~~~~GEOGRAPHY~~~~~~~~~~~~~~~")
+  print("---------------------------------------")
   print("Q"+str(counter)+") "+ "What is the capital of Chile?")
   print("   a) Islamabad")
   print("   b) Santiago")
@@ -64,7 +67,6 @@ while tracker !=1:
   elif answer == "c":
     output = "Wrong. This is the tallest mountain in Italy."
     score -=1
-    
   elif answer == "d":
     output = "Yes, that's right!"
     tracker =1
@@ -110,8 +112,6 @@ while tracker !=1:
   else:
     output = "Please choose a, b, c or d only."
 
-  
-
   print()
   print(output.lower())
   print()
@@ -120,5 +120,154 @@ while tracker !=1:
   print()
 
   
-print("End of quiz!")
   
+counter +=1
+tracker = 0
+
+while tracker !=1:
+  
+  print("Q"+str(counter)+") "+ "Alberta is a province of which country?")
+  print("   a) Algeria")
+  print("   b) Nauru")
+  print("   c) Romania")
+  print("   d) Canada")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Wrong. Alberta is not a province of Algeria."
+    score -=1
+  elif answer == "b":
+    output = "Wrong. Alberta is not a province of Nauru."
+    score -=1
+  elif answer == "c":
+    output = "Wrong. Alberta is not a province of Romania."
+    score -=1
+  elif answer == "d":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  else:
+    output = "Please choose a, b, c or d only."
+
+  print()
+  print(output)
+  print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
+  
+  
+counter +=1
+tracker = 0
+
+while tracker !=1:
+  
+  print("Q"+str(counter)+") "+ "How many countries still have the shilling as currency? Which countries are they?")
+  print("   a) Six - Italy, Israel, Tanzania, Antigua, Barbuda and Rwanda")
+  print("   b) Three - Colombia, The Maldives and Monaco")
+  print("   c) Five - Dominican Republic, Holy See, Tunisia, Mongolia and Albania")
+  print("   d) Four – Kenya, Uganda, Tanzania and Somalia")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Wrong. There are not that many countries."
+    score -=1
+  elif answer == "b":
+    output = "Wrong. There are not that few countries."
+    score -=1
+  elif answer == "c":
+    output = "Wrong. There are not that many countries."
+    score -=1
+  elif answer == "d":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  else:
+    output = "Please choose a, b, c or d only."
+
+  print()
+  print(output)
+  print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
+  
+ 
+counter +=1
+tracker = 0
+
+while tracker !=1:
+  
+  print("Q"+str(counter)+") "+ "Which is the only vowel not used as the first letter in a US State?")
+  print("   a) U")
+  print("   b) I")
+  print("   c) E")
+  print("   d) A")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Wrong. There is a state called Utah."
+    score -=1
+  elif answer == "b":
+    output = "Wrong. There are states called Idaho, Illinois, Indiana and Iowa."
+    score -=1
+  elif answer == "c":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  elif answer == "d":
+    output = "Wrong. There are states called Alabama, Alaska, Arizona and Arkansas."
+    score -=1
+  else:
+    output = "Please choose a, b, c or d only."
+
+  print()
+  print(output)
+  print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
+  
+
+counter +=1
+tracker = 0
+
+while tracker !=1:
+  
+  print("Q"+str(counter)+") "+ "What is the largest country in the world?")
+  print("   a) Canada")
+  print("   b) Russia")
+  print("   c) United States")
+  print("   d) China")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Wrong. This is the second largest country in the world."
+    score -=1
+  elif answer == "b":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  elif answer == "c":
+    output = "Wrong. This is the fourth largest country in the world."
+    score -=1
+  elif answer == "d":
+    output = "Wrong. This is the third largest country in the world."
+    score -=1
+  else:
+    output = "Please choose a, b, c or d only."
+  
+  print()
+  print(output)
+  print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
+
+
+
+print("End of quiz!")
